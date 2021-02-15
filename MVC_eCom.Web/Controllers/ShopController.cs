65dc86931404982d6a84ff06165961316e4fec85
trombone8417 <trombone8417@yahoo.com>
2021-02-16 05:18:18 +0800

    add to cart

---
+++ MVC_eCom.Web/Controllers/ShopController.cs
@@ -0,0 +1,22 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_eCom.Web.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Checkout()
+        {
+            var CartProductsCookie = Request.Cookies["CartProducts"];
+            if (CartProductsCookie != null)
+            {
+                var productIDs = CartProductsCookie.Value;
+            }
+            return View();
+        }
+    }
+}
