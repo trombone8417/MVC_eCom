04bbaf928b5c3588199d9858ffdd944830563b93
trombone8417 <trombone8417@yahoo.com>
2021-02-21 01:05:41 +0800

    Config Migration

--- MVC_eCom.Database/CBContext.cs
+++ MVC_eCom.Database/CBContext.cs
@@ -15,5 +15,6 @@ namespace MVC_eCom.Database
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Config> Configurations { get; set; }
     }
 }
---
+++ MVC_eCom.Entities/Config.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_eCom.Entities
+{
+    public class Config
+    {
+        [Key]
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
